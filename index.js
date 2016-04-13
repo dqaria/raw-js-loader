20e75a0e59f864a3d0ee36b2cd25ebdec5fbba9d
daqi.sdq <daqi.sdq@alibaba-inc.com>
2016-04-13 14:00:01 +0800

    add highlight.js

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
   MIT License http://www.opensource.org/licenses/mit-license.php
   Author Daqi Song
 */
+var hljs = require('highlight.js');
+
 module.exports = function(content, map) {
   this.cacheable && this.cacheable();
   this.value = map.sourcesContent[0];
-  return "module.exports = " + JSON.stringify(map.sourcesContent[0]);
+
+  var sourceCode = map.sourcesContent[0];
+  var highlightedCode = hljs.highlightAuto(sourceCode).value;
+
+  return "module.exports = " + JSON.stringify(highlightedCode);
 }
 module.exports.seperable = true;
